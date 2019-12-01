c27104790c6a186470ff2e1892da1547b395c8f1
sugimotoren <sugimotoren@team-lab.com>
2019-12-02 02:41:28 +0900

    回転処理のコードをオリジナルコードに戻す

--- resizer.go
+++ resizer.go
@@ -590,29 +590,29 @@ func calculateRotationAndFlip(image *C.VipsImage, angle Angle) (Angle, bool) {
 
 	switch vipsExifOrientation(image) {
 	case 6:
-		rotate = D270
+		rotate = D90
 		break
 	case 3:
 		rotate = D180
 		break
 	case 8:
-		rotate = D90
+		rotate = D270
 		break
 	case 2:
 		flip = true
-		rotate = D180
-		break // flip 3
-	case 7:
-		flip = true
-		rotate = D90
-		break // flip 8
-	case 4:
-		flip = true
 		break // flip 1
-	case 5:
+	case 7:
 		flip = true
 		rotate = D270
 		break // flip 6
+	case 4:
+		flip = true
+		rotate = D180
+		break // flip 3
+	case 5:
+		flip = true
+		rotate = D90
+		break // flip 8
 	}
 
 	return rotate, flip
